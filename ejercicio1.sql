ccceaacdbc1358368152d66525ed0aec8cb234c5
Paula Sousa <paulasousa137@gmail.com>
2023-03-27 09:25:13 +0200

    inicializando ejercicio 1

---
+++ ejercicio1.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE  IF NOT EXISTS `TA16_ejercicio1`;
+USE `TA16_ejercicio1`;
+
+DROP TABLE IF EXISTS `fabricantes`;
+CREATE TABLE `fabricantes` (
+  `CODIGO` int NOT NULL,
+  `NOMBRE` varchar(255) NOT NULL,
+  PRIMARY KEY (`CODIGO`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `fabricantes` VALUES (1,'Sony'),(2,'Creative Labs'),(3,'Hewlett-Packard'),(4,'Iomega'),(5,'Fujitsu'),(6,'Winchester');
+
+
+DROP TABLE IF EXISTS `articulos`;
+CREATE TABLE `articulos` (
+  `CODIGO` int NOT NULL,
+  `NOMBRE` varchar(255) NOT NULL,
+  `PRECIO` decimal(10,0) NOT NULL,
+  `FABRICANTE` int NOT NULL,
+  PRIMARY KEY (`CODIGO`),
+  KEY `FABRICANTE` (`FABRICANTE`),
+  CONSTRAINT `articulos_ibfk_1` FOREIGN KEY (`FABRICANTE`) REFERENCES `fabricantes` (`CODIGO`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `articulos` VALUES (1,'Hard drive',240,5),(2,'Memory',120,6),(3,'ZIP drive',150,4),(4,'Floppy disk',5,6),(5,'Monitor',240,1),(6,'DVD drive',180,2),(7,'CD drive',90,2),(8,'Printer',270,3),(9,'Toner cartridge',66,3),(10,'DVD burner',180,2);
+
