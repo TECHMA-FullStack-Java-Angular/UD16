4e7bc7c4ce6c69ffc2b5743c62bfc11dfa70fab8
Paula Sousa <paulasousa137@gmail.com>
2023-03-27 13:08:14 +0200

    inicializando ejercicio 2

---
+++ ejercicio2.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE  IF NOT EXISTS `TA16_ejercicio2`;
+USE `TA16_ejercicio2`;
+
+DROP TABLE IF EXISTS `departamentos`;
+CREATE TABLE `departamentos` (
+  `CODIGO` int NOT NULL,
+  `NOMBRE` varchar(255) NOT NULL,
+  `PRESUPUESTO` decimal(10,0) NOT NULL,
+  PRIMARY KEY (`CODIGO`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `departamentos` VALUES (14,'IT',65000),(37,'Accounting',15000),(59,'Human Resources',240000),(77,'Research',55000);
+
+
+DROP TABLE IF EXISTS `empleados`;
+CREATE TABLE `empleados` (
+  `DNI` int NOT NULL,
+  `NOMBRE` varchar(255) NOT NULL,
+  `APELLIDOS` varchar(255) NOT NULL,
+  `DEPARTAMENTO` int NOT NULL,
+  PRIMARY KEY (`DNI`),
+  KEY `DEPARTAMENTO` (`DEPARTAMENTO`),
+  CONSTRAINT `empleados_ibfk_1` FOREIGN KEY (`DEPARTAMENTO`) REFERENCES `departamentos` (`CODIGO`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `empleados` VALUES (123234877,'Michael','Rogers',14),(152934485,'Anand','Manikutty',14),(222364883,'Carol','Smith',37),(326587417,'Joe','Stevens',37),(332154719,'Mary-Anne','Foster',14),(332569843,'George','O\'Donnell',77),(546523478,'John','Doe',59),(631231482,'David','Smith',77),(654873219,'Zacary','Efron',59),(745685214,'Eric','Goldsmith',59),(845657233,'Luis','López',14),(845657245,'Elizabeth','Doe',14),(845657246,'Kumar','Swamy',14),(845657266,'Jose','Pérez',77);
+
